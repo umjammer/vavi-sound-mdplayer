63701928a008bdf1eb5a4f55e11fc065a022799f
Naohide Sano <namachapanda2005@gmail.com>
2025-02-16 18:48:09 +0900

    [sid] make driver work using JSIDPlay2

---
+++ src/main/java/mdplayer/driver/sid/SidDriver.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package mdplayer.driver.sid;
+
+import java.util.Map;
+
+
+/**
+ * SidDriver.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
+ * @version 0.00 2025-02-16 nsano initial version <br>
+ */
+public interface SidDriver {
+
+    Integer[][] getRegisterFromSid();
+
+    Map<String, Object> getInfo();
+
+    void setSong(int songNo);
+}
